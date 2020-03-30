c19604bf1df28ab7452da2e4616af97016efed42
marco <serranomarco@hotmail.com>
2020-03-30 14:05:47 -0700

    Add more request status codes

--- server.js
+++ server.js
@@ -5,6 +5,38 @@ http.createServer((req, res) => {
         console.log('Inbound \'OK\' request being processed...');
         res.writeHead(200);
         res.end;
+    } else if (req.url === '/Bad-Request') {
+        console.log('Inbound \'Bad-Request\' request being processed...');
+        res.writeHead(400);
+        res.end;
+    } else if (req.url === '/Created') {
+        console.log('Inbound \'Created\' request being processed...');
+        res.writeHead(201);
+        res.end;
+    } else if (req.url === '/Forbidden') {
+        console.log('Inbound \'Forbidden\' request being processed...');
+        res.writeHead(403);
+        res.end;
+    } else if (req.url === '/Found') {
+        console.log('Inbound \'Found\' request being processed...');
+        res.writeHead(302);
+        res.end;
+    } else if (req.url === '/Gateway-Timeout') {
+        console.log('Inbound \'Gateway-Timeout\' request being processed...');
+        res.writeHead(504);
+        res.end;
+    } else if (req.url === '/Internal-Server-Error') {
+        console.log('Inbound \'Internal-Server-Error\' request being processed...');
+        res.writeHead(500);
+        res.end;
+    } else if (req.url === '/Moved-Permanently') {
+        console.log('Inbound \'Moved-Permanently\' request being processed...');
+        res.writeHead(301);
+        res.end;
+    } else if (req.url === '/Unauthorized') {
+        console.log('Inbound \'Unauthorized\' request being processed...');
+        res.writeHead(401);
+        res.end;
     } else {
         console.log('Request: 404 Not Found');
         res.writeHead(404);
