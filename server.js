1177f6e40bf687fcc4a701008393970808165c93
marco <serranomarco@hotmail.com>
2020-03-30 13:58:14 -0700

    Add request functionality

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.end();
+    if (req.url === '/OK') {
+        console.log('Inbound \'OK\' request being processed...');
+        res.writeHead(200);
+        res.end;
+    } else {
+        console.log('Request: 404 Not Found');
+        res.writeHead(404);
+        res.end;
+    }
 }).listen(3000, () => {
     console.log('Listening on port 3000');
 });
